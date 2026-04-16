d50fa8109a88a759219173d956c3d50a64333d33
Oleg Konowalow <oleg.konowalow@onquality.de>
2026-04-16 08:33:24 +0200

    .next and out folder removed from git

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/YanSmet/Work/site_veotronic/wiatech-package/wiatech/app/layout\":[\"static/media/ee40bb094c99a29a-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Zqv02qjP1pORTtJoDKOTfEI/uUTc+BF42xJDMhxc9ic=\"\n}"
